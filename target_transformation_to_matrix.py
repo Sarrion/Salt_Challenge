2019743990893f55ac4a4db5b13f3f09d342b49e
Sarrion <39489130+Sarrion@users.noreply.github.com>
2018-09-05 18:09:20 +0200

    variable names changes

--- target_transformation_to_matrix.py
+++ target_transformation_to_matrix.py
@@ -20,19 +20,21 @@ for i in range(0, len(list_images)):
     image = image.reshape(1, 101**2)
     images[i, :] = image
 images = images/255
+#np.save('images.npy', images)
 
 # =============================================================================
 #     CODE CHUNK TO TRANSFORM TARGET
 # =============================================================================
 target_csv = pd.read_csv('data/train.csv')
+target_csv = target_csv.sort_values(by = "id")
 rle_mask = target_csv.rle_mask.str.split(' ')
 
-target = np.zeros((4000, 10201))
+targets = np.zeros((4000, 10201))
 for i in range(1,4000):
     if(type(rle_mask[i]) != float):
         for j in range(0, len(rle_mask[i]), 2):
             initio = int(rle_mask[i][j]) - 1
             finale =  initio + int(rle_mask[i][j + 1])
-            target[i, initio:finale] = 1
-            
+            targets[i, initio:finale] = 1
+# np.save('targets.npy', targets)        
             
